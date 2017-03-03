beabdac7e3a63230f96b541c8afc6d0f72b0b826
Luke Mattingly <mattingly.luke@yahoo.com>
2017-03-03 13:46:30 -0700

    Added hellow_world to repo

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+    name=[]
+    name.append('Luke')
+    for i in name:
+        print('Hello world, my name is '+i)
